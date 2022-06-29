fb2f6acef553f7d66dbed779fa2b28f63e63fcd8
MCW <aleeshasj@gmail.com>
2022-06-29 20:20:59 +0530

    cleaned the code

--- Class/inheritance.cpp
+++ Class/inheritance.cpp
@@ -1,55 +1,41 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Person{
+class Person {
+ public:
+  int id;
+  char name[10], course[10];
+  int fee;
+  int sal;
 
-    public:
-        int id;
-        char name[10],course[10];
-        int fee;
-        int sal;
+  void feeupdate(int a) { cout << "Fee Paid ...!" << a << endl; }
 
-        void feeupdate(int a){
-            cout<<"Fee Paid ...!"<<a<<endl;
-        }
-
-        void salupdate(int a){
-            cout<<"Salary Credited ...! "<<a<<endl;
-        }
-
+  void salupdate(int a) { cout << "Salary Credited ...! " << a << endl; }
 };
 
-class Student : public Person{
-  public:
-    int y;
-    void setDerived(){
-                cout<<"Derived class"<<endl;
-    }
+class Student : public Person {
+ public:
+  int y;
+  void setDerived() { cout << "Derived class" << endl; }
 };
 
-class Teacher : public Person{
-  public:
-    int y;
-    void setDerived(){
-                cout<<"Derived class"<<endl;
-    }
+class Teacher : public Person {
+ public:
+  int y;
+  void setDerived() { cout << "Derived class" << endl; }
 };
 
+int main() {
+  Person b;
+  Student st;
+  st.fee = 100;
+  st.id = 1;
+  st.feeupdate(st.fee);
 
-int main(){
+  Teacher t;
+  t.sal = 10000;
+  t.id = 44;
+  t.salupdate(t.sal);
 
-    Person b;
-    Student st;
-    st.fee=100;
-    st.id=1;
-    st.feeupdate(st.fee);
-
-    Teacher t;
-    t.sal=10000;
-    t.id=44;
-    t.salupdate(t.sal);
-
-
-    return 0;
+  return 0;
 }
-
