a12b8b35664aacd525fe6d7ab892dcf30fb1f32e
MCW <aleeshasj@gmail.com>
2022-06-29 20:20:24 +0530

    class contructor code

--- Class/constructor.cpp
+++ Class/constructor.cpp
@@ -1,31 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+class Student {
+ public:
+  int rollno, fee;
+  char name[100];
+  Student() {
+    cout << "****Enter following details****" << endl;
+    cout << " Roll no" << endl;
+    cin >> rollno;
+    cout << " Name" << endl;
+    cin >> name;
+    cout << " Fee" << endl;
+    cin >> fee;
+  }
 
-class  Student{
-
-   public:
-    int rollno,fee;
-    char name[100];
-    Student(){
-        cout<<"****Enter following details****"<<endl;
-        cout<<" Roll no"<<endl;
-        cin>>rollno;
-        cout<<" Name"<<endl;
-        cin>>name;
-        cout<<" Fee"<<endl;
-        cin>>fee;
-        }
-
-    void printDetails(){
-        cout<<"****Details****"<<endl;
-        cout<<name<<endl<<rollno<<endl<<fee<<endl;
-    }
+  void printDetails() {
+    cout << "****Details****" << endl;
+    cout << name << endl << rollno << endl << fee << endl;
+  }
 };
 
-int main(){
-
-    Student S;
-    S.printDetails();
-    return 0;
+int main() {
+  Student S;
+  S.printDetails();
+  return 0;
 }
