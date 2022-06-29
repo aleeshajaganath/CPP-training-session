f6fa7d956d5251bf9d35187d29b736fe07ddba5a
MCW <aleeshasj@gmail.com>
2022-06-29 20:45:18 +0530

    deleted unwanted

--- callback_fun.c
+++ callback_fun.c
@@ -1,19 +1,16 @@
 // A simple C program to demonstrate callback
 #include<stdio.h>
 
-void A(int x, int y)
-{
+void A(int x, int y){
 	printf("I am function A\n");
 }
 
 // callback function
-void B(int x, int y,void (*ptr)(int x,int y))
-{
+void B(int x, int y,void (*ptr)(int x,int y)){
 	(*ptr) (x,y); // callback to A
 }
 
-int main()
-{
+int main(){
 	void (*ptr)(int x ,int y) = &A;
 	int x=5,y=9;
 	// calling function B and passing
