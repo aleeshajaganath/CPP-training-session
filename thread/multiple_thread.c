107eb98e60d2280c7f8f15636f1ee32a687b8597
MCW <aleeshasj@gmail.com>
2022-06-29 20:44:29 +0530

    cleaned the code

--- thread/multiple_thread.c
+++ thread/multiple_thread.c
@@ -1,27 +1,25 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> //Header file for sleep(). man 3 sleep for details.
-#include <pthread.h>
+#include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 
-void* Justprint(void *threadID){
-    long tid;
-    tid= (long) threadID;
-    printf("\t thread id no is %ld ",tid);
-
+void *Justprint(void *threadID) {
+  long tid;
+  tid = (long)threadID;
+  printf("\t thread id no is %ld ", tid);
 }
-int main(){
-    int count=10;
-    pthread_t threads[10];
+int main() {
+  int count = 10;
+  pthread_t threads[10];
 
-    for (long i = 0; i < count; i++)    {
-        printf(" \n In main creating thread  %ld \n",i);
-        int rc=pthread_create(&threads[i], NULL,Justprint, (void *) i);
-        if (rc){
-            printf("ERROR CODE %d",rc);
-            exit(-1);
-        }
-
+  for (long i = 0; i < count; i++) {
+    printf(" \n In main creating thread  %ld \n", i);
+    int rc = pthread_create(&threads[i], NULL, Justprint, (void *)i);
+    if (rc) {
+      printf("ERROR CODE %d", rc);
+      exit(-1);
     }
-    
-    return 0;
+  }
+
+  return 0;
 }
--- thread/thread.c
+++ thread/thread.c
@@ -1,35 +1,34 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> //Header file for sleep(). man 3 sleep for details.
-#include <pthread.h>
+#include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
 
-
-void *myThreadFun(void *vargp){
-	sleep(1);
-	printf("Printing  from Thread \n");
-	return NULL;
+void *myThreadFun(void *vargp) {
+  sleep(1);
+  printf("Printing  from Thread \n");
+  return NULL;
 }
 
-int main(){
-	pthread_t thread_id;//thread object creation
-	printf("Before Thread\n");
+int main() {
+  pthread_t thread_id;  // thread object creation
+  printf("Before Thread\n");
 
-    /*  A normal C function that is executed as a thread
-        when its name is specified in pthread_create()
-        pthread_create(pthread_t obj address, NULL, function pointer, NULL);*/
-	pthread_create(&thread_id, NULL, myThreadFun, NULL);
+  /*  A normal C function that is executed as a thread
+      when its name is specified in pthread_create()
+      pthread_create(pthread_t obj address, NULL, function pointer, NULL);*/
+  pthread_create(&thread_id, NULL, myThreadFun, NULL);
 
-    /*  pthread_join - join with a terminated thread
-        pthread_join(pthread_t obj,NULL)
-        On success, pthread_join() returns 0; on error, it returns an
-        error number. */
-	pthread_join(thread_id, NULL);
-	printf("After Thread\n");
-	exit(0);
+  /*  pthread_join - join with a terminated thread
+      pthread_join(pthread_t obj,NULL)
+      On success, pthread_join() returns 0; on error, it returns an
+      error number. */
+  pthread_join(thread_id, NULL);
+  printf("After Thread\n");
+  exit(0);
 }
 /*
         run thread in terminal
-        1. gcc -o thread thread.c -pthread 
+        1. gcc -o thread thread.c -pthread
         2. ./thread
-        
+
 */
