6455d3642cf331d84dffbf2bb4666943da449fb0
MCW <aleeshasj@gmail.com>
2022-06-29 20:26:54 +0530

    cleaned the code

--- Pointers/lecture8.cpp
+++ Pointers/lecture8.cpp
@@ -1,17 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-int main(){
-
-    // void pointer
-    int x=10;
-    void* ptr;
-    ptr=&x;
-
-    int *ptrr = (int*)ptr;
-    cout<<*ptrr<<endl;
-
-    // Function pointers same as in first.cpp
-    return 0;
+int main() {
+  // void pointer
+  int x = 10;
+  void* ptr;
+  ptr = &x;
+  int* ptrr = (int*)ptr;
+  cout << *ptrr << endl;
+  return 0;
 }
--- Pointers/voidpointer.cpp
+++ Pointers/voidpointer.cpp
@@ -4,17 +4,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int a = 10;
-	char b = 'x';	
-    void* ptr = &a;   
-    cout << *(int *)ptr << endl;
-    int n = 4;
-    double pi = 3.14;
-    int *pn = &n ; 
-    double *ppi = &pi;
-    ppi = (double*)pn;
-    cout<< "explicit convertion *pii " <<*ppi << endl;
-
+int main() {
+  int a = 10;
+  char b = 'x';
+  void *ptr = &a;
+  cout << *(int *)ptr << endl;
+  int n = 4;
+  double pi = 3.14;
+  int *pn = &n;
+  double *ppi = &pi;
+  ppi = (double *)pn;
+  cout << "explicit convertion *pii " << *ppi << endl;
 }
-
