b049a0b346e9ca97a4fe47240aed0bcded262d05
MCW <aleeshasj@gmail.com>
2022-07-06 11:38:11 +0530

    .h file upldated

--- Assignment/results/symtable.h
+++ Assignment/results/symtable.h
@@ -7,7 +7,7 @@ typedef SymTable_t iNode;
 
 // template <void>struct SymTable_t* x;
 SymTable_t* SymTable_new ( );
-void Print();
+void Print(SymTable_t *);
 void SymTable_free (SymTable_t *);
 int SymTable_getLength (SymTable_t *oSymTable);
 int SymTable_put (SymTable_t *,const char *pcKey,const void *pvValue);
@@ -17,7 +17,6 @@ void *SymTable_get (SymTable_t *, const char *pcKey);
 void *SymTable_remove (SymTable_t *,const char *pcKey);
 void SymTable_map (SymTable_t *,
                   void (*pfApply) (const char *pcKey,
-                  const void *pvValue),
-                  const void *pvExtra);
+                  const void *pvValue));
 void display(const char *, const void *);
 #endif
